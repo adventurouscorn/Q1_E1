5852e7c86d5d66024f82ffab9979e7a26b29c8e5
Philler Siy <b2026pavsiy@pshs.edu.ph>
2023-09-03 13:41:28 +0800

    First Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    int x = 5;
+    public static void main (String args[]){
+        Main test = new Main();
+        test.x = 40;
+        System.out.println(test.x);
+    }
+}
