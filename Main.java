5cdab96437db24aae9f236c6268b8fcc063f1235
Philler Siy <b2026pavsiy@pshs.edu.ph>
2023-09-03 13:47:27 +0800

    New Features

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
     int x = 5;
+    int y = 5;
     public static void main (String args[]){
         Main test = new Main();
         test.x = 40;
+        test.y = 12;
         System.out.println(test.x);
+        System.out.println(test.y);
     }
 }
